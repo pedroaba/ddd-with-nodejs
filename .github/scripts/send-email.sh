9dd676683e6c6fa30966c29e1628e6a4292475b7
Pedro Augusto B. Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-09-30 00:28:57 -0300

    fix: send mail script

--- .github/scripts/send-email.sh
+++ .github/scripts/send-email.sh
@@ -1,3 +1,4 @@
 sudo apt-get install mailutils
+ls
 zip -r reports.zip . -i coverage/*
 echo "Pipeline executado com o commit '$COMMIT_MESSAGE', segue os relatórios em anexo." | mail -s "Pipeline executado com sucesso" -A reports.zip "$EMAIL_TO_SEND"
