2af17cad04099230c722343c217a3d44300e0216
Pedro Augusto B. Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-09-30 00:50:13 -0300

    fix: removing 'echo' from send mail scripts

--- .github/scripts/send-email.sh
+++ .github/scripts/send-email.sh
@@ -1,6 +1,3 @@
 sudo apt-get install mailutils
 zip -r reports.zip . -i coverage/*
-ls
-echo "EMAIL_TO_SEND: $EMAIL_TO_SEND"
-echo "COMMIT_MESSAGE: $COMMIT_MESSAGE"
 echo "Pipeline executado com o commit '$COMMIT_MESSAGE', segue os relatórios em anexo." | mail -s "Pipeline executado com sucesso" -A reports.zip "$EMAIL_TO_SEND"
