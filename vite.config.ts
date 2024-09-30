9344e2a9e04e9729bc97fe42e9f41809d3393082
Pedro Augusto B. Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-09-29 23:20:40 -0300

    chore: configure test pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vitest-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
-  test: {
-    globals: true,
-  },
+    plugins: [tsConfigPaths()],
+    test: {
+        globals: true
+    }
 })
